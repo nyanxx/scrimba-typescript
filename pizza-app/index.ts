bff7761e51b91cdbca1284d801cdd9f1d48553b9
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-28 15:14:51 +0530

    chore: add Pizza type

--- pizza-app/index.ts
+++ pizza-app/index.ts
@@ -1,5 +1,16 @@
 // Static values
-const menu = [
+
+/**
+ * Challenge: Create a Pizza object type. It should include a `name`
+ * and a `price` property.
+ */
+
+type Pizza = {
+  name: string,
+  price: number
+}
+
+const menu: Pizza[] = [
   { name: "Margherita", price: 8 },
   { name: "Pepperoni", price: 10 },
   { name: "Hawaiian", price: 10 },
@@ -10,18 +21,16 @@ let cashInRegister: number = 100;
 let orderQueue: { id: number, pizza: object, status: string }[] = [];
 let globalID: number = 1;
 
+/**
+ * Challenge: teach TS that the pizzaObj is supposed to be a Pizza type.
+ * Then like before, look through the code to see if there are any new
+ * TS warnings to deal with (😉), and fix those issues
+ */
 
-function addNewPizza(obj: { name: string, price: number }) {
+function addNewPizza(obj: Pizza) {
   menu.push(obj);
   //   console.log(`${obj.name} pizza added to menu!`);
 }
-
-/** addNewPizza(name: string, price: number) */
-// function addNewPizza(name: string, price: number) {
-// menu.push({ name: name, price: price });
-//   console.log(`${name} pizza added to menu!`);
-// }
-
 
 function placeOrder(pizzaName: string) {
   const pizzaOrdered = menu.find((obj) => obj.name === pizzaName);
