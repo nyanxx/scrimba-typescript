e8ea5b83693ef27336613c8dcdeeefb7da9a1a02
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-01-30 10:59:36 +0530

    refactor: from js to ts

---
+++ assembly-endgame-react-typescipt/src/assets/languages.ts
@@ -0,0 +1,54 @@
+
+type Language = {
+  name: string,
+  backgroundColor: string
+  fontColor: string
+}
+
+export default [
+  {
+    name: "HTML",
+    backgroundColor: "#E2680F",
+    fontColor: "#F9F4DA",
+  },
+  {
+    name: "CSS",
+    backgroundColor: "#328AF1",
+    fontColor: "#F9F4DA",
+  },
+  {
+    name: "JavaScript",
+    backgroundColor: "#F4EB13",
+    fontColor: "#1E1E1E",
+  },
+  {
+    name: "React",
+    backgroundColor: "#2ED3E9",
+    fontColor: "#1E1E1E",
+  },
+  {
+    name: "TypeScript",
+    backgroundColor: "#298EC6",
+    fontColor: "#F9F4DA",
+  },
+  {
+    name: "Node.js",
+    backgroundColor: "#599137",
+    fontColor: "#F9F4DA",
+  },
+  {
+    name: "Python",
+    backgroundColor: "#FFD742",
+    fontColor: "#1E1E1E",
+  },
+  {
+    name: "Ruby",
+    backgroundColor: "#D02B2B",
+    fontColor: "#F9F4DA",
+  },
+  {
+    name: "Assembly",
+    backgroundColor: "#2D519F",
+    fontColor: "#F9F4DA",
+  },
+] as Language[];
